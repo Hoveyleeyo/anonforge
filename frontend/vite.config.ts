d00fe7f2d296a7888b120388acf7db340489773c
hovey <hoveleeyo@gmail.com>
2026-05-03 00:09:57 +0800

    配置反向代理，让客户端组件中发起的网络请求，如果以或者开头的，全部转发到服务端

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from "node:path"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // 配置路径别名，使用 '@' 代表 'src' 目录
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    // 配置开发服务器的代理，将 API 请求转发到后端服务器
+    proxy: {
+      // 将以 /api 开头的请求代理到 http://localhost:3000
+      '/api': "http://localhost:8088/api",
+      "/oss": "http://localhost:8088/oss"
+    }
+  }
 })
